944983a50b80d06eafadcc0b2229cc578d729821
Isaac Bankier <isaacbankier@protonmail.com>
2022-05-18 11:44:49 +1000

    Added registration

--- Implementation/app/Login.hs
+++ Implementation/app/Login.hs
@@ -24,6 +24,12 @@ login = do
     u <- param' "username"
     p <- param' "password"
     loginAction u p
+  prehook loggedOutHook $ post "/register" $ do
+    n <- param' "name"
+    u <- param' "username"
+    p <- param' "password"
+    p2 <- param' "password2"
+    registerAction n u p p2
 
 loginPage :: Bool -> Handler (HVect xs) a
 loginPage failed = mkSite $ do
@@ -31,17 +37,17 @@ loginPage failed = mkSite $ do
     div_ [id_ "login"] $ do
       h1_ "Login"
       when failed $ h2_ [id_ "login-failure"] "Login failed"
-      form_ [method_ "post"] $ do
+      form_ [method_ "post", action_ "/login"] $ do
         input_ [type_ "email", placeholder_ "Email", name_ "username"]
         input_ [type_ "password", placeholder_ "Password", name_ "password"]
         input_ [type_ "submit", value_ "Login"]
     div_ [id_ "register"] $ do
       h1_ "Register"
-      form_ [method_ "post"] $ do
-        input_ [type_ "email", placeholder_ "Name", name_ "username"]
+      form_ [method_ "post", action_ "/register"] $ do
+        input_ [type_ "text", placeholder_ "Name", name_ "name"]
         input_ [type_ "email", placeholder_ "Email", name_ "username"]
         input_ [type_ "password", placeholder_ "Password", name_ "password"]
-        input_ [type_ "password", placeholder_ "Password, Again", name_ "password"]
+        input_ [type_ "password", placeholder_ "Password, Again", name_ "password2"]
         input_ [type_ "submit", value_ "Register"]
 
 loginAction :: T.Text -> T.Text -> Handler (HVect xs) a
@@ -57,6 +63,18 @@ loginAction u p = do
       writeSession $ Just $ IsEmployee u'
       redirect "/"
     _ -> redirect "/loginFailed"
+
+registerAction :: T.Text -> T.Text -> T.Text -> T.Text -> Handler (HVect xs) a
+registerAction n u p p2 = if p == p2 then do
+  runSqlStmt "INSERT INTO users (email, name, password, role) VALUES (?, ?, ?, \"customer\")" (u, n, p)
+  userQ <- runSqlQuery "SELECT * FROM users WHERE email = ? AND password = ?" [u, p]
+  case userQ of
+    [u'@(User _ _ _ _ "customer")] -> do
+      sessionRegenerateId
+      writeSession $ Just $ IsCustomer u'
+      redirect "/"
+    _ -> redirect "/loginFailed"
+  else redirect "/loginFailed"
 
 logout :: Handler (HVect xs) a
 logout = do
